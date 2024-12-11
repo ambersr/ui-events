4f913182ef7bbcf6e7e3e7a54140cea44fb1e0b8
ambersr <amber.schalker@hva.nl>
2024-12-11 15:05:44 +0100

    Laatste code aangepast

--- main.js
+++ main.js
@@ -1,33 +1,3 @@
-/*
-   De eerste interactie is al voor jullie uitgewerkt
-   Als je op het 12e linkje klikt (“Interaction”), springt deze omhoog
-*/
-
-// Stap 1: selecteer het 12e linkje, en sla deze op in een variabele
-let interaction = document.querySelector('a:nth-of-type(12)')
-
-// Stap 2: voeg de (click) event listener toe aan de link, met een callback functie
-// In dit geval wordt de jumpHandler functie aangeroepen, zodra je op het linkje klikt
-interaction.addEventListener('click', jumpHandler)
-
-// Deze jumpHandler functie staat klaar voor als we 'm aanroepen; deze wordt dus
-// _niet_ meteen bij het laden van de pagina aangeroepen
-function jumpHandler() {
-
-  // Stap 3: toggle een class op het element (zodra er dus op een link wordt geklikt)
-  interaction.classList.toggle('jump')
-}
-
-// Extraatje, waardoor de class weer weggehaald wordt zodra de animatie afgelopen is
-interaction.addEventListener('animationend', jumpHandler)
-
-
-
-
-
-
-
-
 // Link 1 Knop draggen naar droparea (Dragstart & dragend)
 const dragLink = document.querySelector('a:nth-of-type(1)');
 const dropArea = document.querySelector('.drop-area');
@@ -174,6 +144,15 @@ fadeLink.addEventListener('blur', () => {
       }
     });
 
+// Link 11 Bibberen bij klik knop (click)
 
+let interaction = document.querySelector('a:nth-of-type(11)')
 
+interaction.addEventListener('click', jumpHandler)
+
+function jumpHandler() {
+  interaction.classList.toggle('jump')
+}
+
+interaction.addEventListener('animationend', jumpHandler)
 
